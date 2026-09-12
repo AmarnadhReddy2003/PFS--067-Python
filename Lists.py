d1a9fbe2cca02fe366c7ceaad83e6d9015058c1f
Amarnadh <amarnadh84650@gmail.com>
2026-09-12 11:46:37 +0530

    List

---
+++ Lists.py
@@ -0,0 +1,29 @@
+# #Lists
+# # Created by using []-square brackets
+# # In python both lists and arrays are same ,we use same functions or operations in them.
+# # List is an ordered data structured, it is mutable and collection of objects
+# Ram = [90,80,50,92,78]
+# print(Ram)
+# # Characters of string
+# # 1. Propered order is maintained
+# # 2. Allows duplicates
+# # 3. Mutable- can be changed after it was created
+# #list_name[index] = new_value
+# Ram[0]= 100
+# print(Ram)
+# # List can contin different types 
+# student=['Ravi','Ayodhya',10]
+# print(student)
+# # Adding elements in list
+# # append() method is used to add the elements in the list
+# student.append(80)
+# print(student)
+# # Extend()- to join two lists
+# a= [10,20,30]
+# b= [40,50,60]
+# a.extend(b)
+# print(a)
+# a.pop()
+# print(a)
+# a.remove(40)
+# print(a)
