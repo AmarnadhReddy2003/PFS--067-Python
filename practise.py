0ab5f8c2e6e4b03979d6f45b97cf71a2ecab771f
Amarnadh <amarnadh84650@gmail.com>
2026-09-23 10:55:37 +0530

    Add practise program

---
+++ practise.py
@@ -0,0 +1,74 @@
+# # Employee performance evaluation
+# employe={'name':input('Enter the name: '),
+#          'No_of_Projects_Completed':int(input('Enter the no of projects you completed: ')),
+#           'Performance_Rating':int(input('Enter your project Rating: '))
+#           }
+
+
+# if employe.get('Performance_Rating')>=4 and employe['No_of_Projects_Completed']>=5:
+#     print('Excellent')
+# elif employe.get('Performance_Rating')>=3 and employe['No_of_Projects_Completed']>=3:
+#     print('Good')
+# elif employe.get('Performance_Rating')>=2:
+#     print('Needs Improvement')
+# else:
+#     print("Keep going don't give up")
+
+# # Number Classification
+# n=int(input('Enter the Number: '))
+# if n>0:
+#     if n%2==0:
+#         print(f'{n} is Positive and Even ')
+#     else:
+#         print(f'{n} is Positive and Odd ')
+# elif n<0:
+#     if n%2==0:
+#         print(f'{n} is Negative and Even ')
+#     else:
+#         print(f'{n} is Negative and Odd ')
+# else:
+#     print(f'{n} is Zero')
+
+# # Shopping Discount
+# price=int(input('Enter the Price of the Product: '))
+# member=input('Are you a member(Yes/No): ')
+# if price>=10000:
+#     if member=='Yes':
+#         print('Discount is 25%')
+#         discount=price*0.25
+#         total=price-discount
+#         print(total)
+#     else:
+#         print('Discount is 20%')
+#         discount=price*0.20
+#         total=price-discount
+#         print(total)
+# elif price>=5000:
+#     if member=='Yes':
+#         print('Discount is 15%')
+#         discount=price*0.15
+#         total=price-discount
+#         print(total)
+#     else:
+#         print('Discount is 10%')
+#         discount=price*0.1
+#         total=price-discount
+#         print(total)
+# elif price<5000 and price>3000:
+#     print('Discount is 5%')
+#     discount=price*0.5
+#     total=price-discount
+#     print(total)
+# elif price<=3000:
+#     print(" You Don't have any Discount")
+#     print(price)
+
+
+# # List Element Validation
+# n1=int(input('Enter the 1 element in List: '))
+# n2=int(input('Enter the 2 element in List: '))
+# n3=int(input('Enter the 3 element in List: '))
+# n4=int(input('Enter the 4 element in List: '))
+# n5=int(input('Enter the 5 element in List: '))
+# l=[n1,n2,n3,n4,n5]
+# if l[0]
